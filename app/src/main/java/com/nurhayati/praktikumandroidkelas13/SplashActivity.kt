bd5d30fa6b2a813b2471b4a309e65ea6a7ddab4d
Nurhayati <Ayyaweela24@gmail.com>
2020-12-26 21:32:57 +0800

    Intent Update

---
+++ app/src/main/java/com/nurhayati/praktikumandroidkelas13/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.nurhayati.praktikumandroidkelas13
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this, StudentActivity::class.java)
+            startActivity(i)
+        }, 3000)
+    }
+}
--- app/src/main/java/com/nurhayati/praktikumandroidkelas13/StudentActivity.kt
+++ app/src/main/java/com/nurhayati/praktikumandroidkelas13/StudentActivity.kt
@@ -3,17 +3,38 @@ package com.nurhayati.praktikumandroidkelas13
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.widget.Button
+import android.widget.EditText
 
 class StudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_student)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            val i = Intent(this, StudentActivity::class.java)
+
+        val etNPM = findViewById<EditText>(R.id.etNpm)
+        val etNama = findViewById<EditText>(R.id.etNama)
+        val etIpk = findViewById<EditText>(R.id.etIpk)
+        val etJumlahMataKuliah = findViewById<EditText>(R.id.etJumlahMataKuliah)
+
+        val btIntent1 = findViewById<Button>(R.id.btIntent1)
+        val btIntent2 = findViewById<Button>(R.id.btIntent2)
+
+        btIntent1.setOnClickListener {
+            val npm = etNPM.text.toString()
+            val nama = etNama.text.toString()
+            val ipk = etIpk.text.toString().toDouble()
+            val jumlahMataKuliah = etJumlahMataKuliah.text.toString().toInt()
+
+            val i = Intent(this, StudentIntentActivity::class.java)
+            i.putExtra("npm", npm)
+            i.putExtra("nama", nama)
+            i.putExtra("ipk", ipk)
+            i.putExtra("jumlahMataKuliah", jumlahMataKuliah)
+
             startActivity(i)
-        }, 3000)
+
+        }
+
     }
 }
